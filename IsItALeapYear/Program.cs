30f0594ea79471ff50ecfe281bd2b36a6c69739b
Rupinderjeet <65835380+Rupinderj@users.noreply.github.com>
2021-07-04 14:42:15 -0400

    Add files via upload

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public Boolean IsItALeapYear(int a)
+        {
+           
+            if ((a % 4) == 0)
+            {
+                return true;
+            }
+            else if ((a % 400) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
