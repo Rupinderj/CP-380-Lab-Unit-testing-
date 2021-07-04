cfa54ec7d1c816dd8299d81be312a1e8abfc80a6
Rupinderjeet <65835380+Rupinderj@users.noreply.github.com>
2021-07-04 14:58:33 -0400

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Year_2001_Is_not_a_leap_year()
+
+{
+
+ // Arrange
+
+ var p = new Program();
+
+ var expected = false;
+
+ // Act
+
+ var actual = p.IsItALeapYear(2001);
+
+ // Assert
+
+ Assert.AreEqual(expected, actual);
+
+}
+    }
+}
